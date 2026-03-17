cdb462b052ae90eda41ee5700ef61066e35126cd
muhammedansilriyas <muhammedansilriyas@gmail.com>
2026-03-17 10:42:50 +0530

    Time: 1 ms (91.99%), Space: 56.6 MB (35.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+
+    let map = {
+        ")":"(",
+        "}":"{",
+        "]":"[",
+    };
+
+    for(let char of s ){
+        if(char ==="(" || char === "{" || char === "["){
+            stack.push(char);
+        }else{
+            let top = stack.pop();
+            if(top !==map[char]){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
